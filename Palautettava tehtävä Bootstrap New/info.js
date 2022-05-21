eec3edd370fe0f84d5bdff0935539a7534c74dbd
Ville Närhi <villenarhi@hotmail.fi>
2022-05-21 15:54:31 +0300

    changed h3 with js

--- Palautettava tehtävä Bootstrap New/info.js
+++ Palautettava tehtävä Bootstrap New/info.js
@@ -1,4 +1,13 @@
 function change() {
-    let firstUl = document.querySelector("ul");
-    firstUl.textContent = "https://fi.wikipedia.org/wiki/Auston_Matthews"
+    let textAuston = document.querySelector("#wiki1");
+    textAuston.textContent = "https://fi.wikipedia.org/wiki/Auston_Matthews"
+
+    let textSource = document.querySelector("#teksti");
+    textSource.textContent = "Tekstit"
+
+    let photoSource = document.querySelector("#kuvat");
+    photoSource.textContent = "Kuvat"
+
+    let videoSource = document.querySelector("#videot");
+    videoSource.textContent = "Videot"
 }
