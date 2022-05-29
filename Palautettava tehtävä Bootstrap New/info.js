5286b89d22dbe8db96b53683d1893a4dfb8f5d5a
Ville Närhi <villenarhi@hotmail.fi>
2022-05-29 15:26:23 +0300

    added rest js

--- Palautettava tehtävä Bootstrap New/info.js
+++ Palautettava tehtävä Bootstrap New/info.js
@@ -61,3 +61,18 @@ function add() {
     photos.insertBefore(textWiki, wikipedia);
     
 }
+
+// RESTin lisäys
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'healthruwords.p.rapidapi.com',
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+	}
+};
+
+fetch('https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
